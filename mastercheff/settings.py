2d29e4ff6a70405337d19b7a4dc5146d3b752729
andrenav93 <andrenavar10@gmail.com>
2016-05-24 16:17:37 -0400

    Aprendiendo a disenar

--- mastercheff/settings.py
+++ mastercheff/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR, "static_in_pro","our_static"),
+)
+
 
 # Formato de lectura de fechas
 DATE_INPUT_FORMATS = ('%d-%m-%Y')
--- pedidos/views.py
+++ pedidos/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 # Aqui van las views/vistas
 
 def base(request):
-    return render(request, 'base.html')
+    return render(request, 'base_2.html')
 
 def indice(request):
     return render(request, 'indice.html')
