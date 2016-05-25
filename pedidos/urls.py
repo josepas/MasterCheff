f362ec197a6df6f02991eb1c0652adaec5b5b779
andrenav93 <andrenavar10@gmail.com>
2016-05-24 22:22:47 -0400

    Cambios de prueba

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -3,9 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
     url(r'^$', views.login, name='registro'),
     url(r'^$', views.indice, name='registro'),
     url(r'^registro/', views.registro, name='registro' )
-
 ]
