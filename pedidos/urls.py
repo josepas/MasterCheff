1d8a1557c44082d916a67f8e096689ca71630240
Jose Pascarella Quijada <josepas979@gmail.com>
2016-05-24 22:15:17 -0400

    fix para las url

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.indice, name='registro'),
+    url(r'^$', views.indice, name='indice'),
     url(r'^registro/$', views.registro, name='registro' ),
     url(r'^registro/restaurante/$', views.registroRestaurante, name='regRestaurante')
 
