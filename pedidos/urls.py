670cd8a9051ca1edc55651981ecd14f0adcd0bc5
andres <andrenavar10@gmail.com>
2016-06-02 20:59:51 -0400

    Iniciando la rama Tester

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 
 	url(r'^editarPerfil/', views.editarPerfil, name='editarPerfil'),
 	url(r'^perfil/', views.perfil, name='perfil'),
-    url(r'^$', views.indice, name='indice'),
+    url(r'^$', views.indice , name='indice'),
     url(r'^login/', views.login, name='login'),
     url(r'^logout/', views.logout_view, name='logout'),
     url(r'^registro/', views.registro, name='registro'),
