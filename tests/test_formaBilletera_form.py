a39f5a543538b99957065979072d2fe4df8e4943
Christopher <christo.8.16@gmail.com>
2016-06-09 11:42:59 -0400

    primeros test de billetera, pero falta acomodar el modal de la billetera

--- tests/test_formaBilletera_form.py
+++ tests/test_formaBilletera_form.py
@@ -6,4 +6,45 @@ from pedidos.forms import (
 
 # Create your tests here.
 class FormaBilleteraTestCase(TestCase):
-    pass
+
+    # malicia
+    def test_FormaBilletera_vacio(self):
+        form_data = {}
+        form = FormaBilletera(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    #Borde
+    def test_FormaBilletera_unCampo(self):
+        form_data = {
+            'pin': '1234',
+            'saldo': None
+        }
+        form = FormaBilletera(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    #Borde
+    def test_FormaBilletera_DosCampos(self):
+        form_data = {
+            'pin': '1234',
+            'saldo': 100
+        }
+        form = FormaBilletera(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    #Malicia
+    def test_FormaBilletera_PinInvalido(self):
+        form_data = {
+            'pin': 'ACVC',
+            'saldo': 100
+        }
+        form = FormaBilletera(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    # Malicia
+    def test_FormaBilletera_SaldoInvalido(self):
+        form_data = {
+            'pin': '1234',
+            'saldo': 'ACBSA'
+        }
+        form = FormaBilletera(data=form_data)
+        self.assertFalse(form.is_valid())
